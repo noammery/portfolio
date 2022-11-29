0663d5c50ca6376d323c206f68dfd897d9c3c3fa
noammery <noammery55@gmail.com>
2022-11-29 15:47:33 +0200

    links fixed

--- src/components/Carousle.jsx
+++ src/components/Carousle.jsx
@@ -7,31 +7,17 @@ const Carousle = () => {
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
             Public Auction
           </h5>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-sm">
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-xs">
             Public Auction site, where you can Register/Login to a secured user,
-            place bids, add products to sale and browse different products! The
-            site is secured with JWT.
+            place bids, add products to sale and browse different products!
           </p>
           <a
             target="_blank"
             rel="noreferrer"
-            href="https://github.com/noammery/Bids"
-            className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            href="https://remarket-client.netlify.app/"
+            className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
           >
-            Read more
-            <svg
-              aria-hidden="true"
-              className="ml-2 -mr-1 w-4 h-4"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
+            To The Site
           </a>
         </div>
       </div>
@@ -49,23 +35,10 @@ const Carousle = () => {
           <a
             target="_blank"
             rel="noreferrer"
-            href="https://github.com/noammery/policy-client"
-            className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            href="https://dimona-end-project.netlify.app/"
+            className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
           >
-            Read more
-            <svg
-              aria-hidden="true"
-              className="ml-2 -mr-1 w-4 h-4"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
+            To The Site
           </a>
         </div>
       </div>
@@ -75,7 +48,7 @@ const Carousle = () => {
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
             BTW, Messanger Site
           </h5>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-sm">
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-xs">
             A messanger site, Users, Private rooms, Full encrypting, Chat
             history for every room and a Bot that learns your questions and
             Answers itself!
@@ -84,22 +57,9 @@ const Carousle = () => {
             target="_blank"
             rel="noreferrer"
             href="https://btw-messanger.netlify.app/"
-            className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
           >
-            Read more
-            <svg
-              aria-hidden="true"
-              className="ml-2 -mr-1 w-4 h-4"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
+            To The Site
           </a>
         </div>
       </div>
